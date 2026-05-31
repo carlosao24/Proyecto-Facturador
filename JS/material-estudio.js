8a7a7c482274f1872cffbf886c360e382ef5338c
carlosao24 <carloslopez21trabajos@gmail.com>
2026-05-31 18:53:58 -0500

    Cambio de conceptos en html

--- JS/material-estudio.js
+++ JS/material-estudio.js
@@ -1,12 +1,12 @@
 // Función para calcular el precio total multiplicando precio por cantidad
 function calcularPrecioTotal(){
     // Guardamos los valores que el usuario escribió en los cuadros de texto
-    let precioUnitario = recuperarInt("precioUnitario");
+    let precioUnitario = recuperarFloat("precioUnitario");
     let cantidad = recuperarInt("cantidad");
 
     // Hacemos la operación matemática
     let precioTotal = precioUnitario * cantidad;
 
     // Mostramos el resultado en la pantalla dentro del espacio con id 'mensaje1'
-    document.getElementById("mensajeAritmetica").innerHTML = "precioTotal: " + precioTotal;
+    document.getElementById("mensajeProducto").innerHTML = "precioTotal: " + precioTotal.toFixed(2);
 }
