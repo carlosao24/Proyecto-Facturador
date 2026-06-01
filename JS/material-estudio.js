34b9b3b747dc8a59b5492d4d125e08d7394d4d6a
carlosao24 <carloslopez21trabajos@gmail.com>
2026-05-31 20:41:29 -0500

    Agregando concepto IVA

--- JS/material-estudio.js
+++ JS/material-estudio.js
@@ -11,3 +11,15 @@ function calcularSubtotal() {
     document.getElementById("mensajeSubtotal").innerHTML = "Subtotal: " + subtotal.toFixed(2);
 }
 
+// Función para calcular los impuestos y totales finales de la factura
+function calcularIVA(){
+    let iva = 15; // El porcentaje del iva normal es 15%
+    let subTotal = recuperarFloat("idSubTotal");
+    let ivaSubTotal;
+    let total;
+    // Si es un cliente común, el iva se calcula con el 15% original
+    ivaSubTotal = (subTotal * iva) / 100;
+    total = subTotal + ivaSubTotal;
+    document.getElementById("mensajeIVA").innerHTML = "El iva es de: " + ivaSubTotal.toFixed(2);
+    document.getElementById("mensajeTotal").innerHTML = "El total es de: " + total.toFixed(2);
+}
